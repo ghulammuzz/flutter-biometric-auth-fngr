58c54620a1ff48a0000370a29d001d8d14720479
ghulammuzz <ghulamxiaomi@gmail.com>
2022-09-07 21:54:40 +0700

    add fingrtprint scanner

---
+++ lib/home.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+
+class home extends StatefulWidget {
+  const home({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  final LocalAuthentication auth = LocalAuthentication();
+
+  String _authorized = 'Not Authorized';
+  bool _isAuthenticating = false;
+
+  Future<void> _authenticate() async {
+    bool authenticated = false;
+    try {
+      setState(() {
+        _isAuthenticating = true;
+        _authorized = 'Not Authorized';
+      });
+      authenticated = await auth.authenticate(
+          localizedReason: 'Please authenticate to show account balance',
+          options: AuthenticationOptions(
+            useErrorDialogs: true,
+            stickyAuth: true,
+          ));
+      setState(() {
+        _isAuthenticating = false;
+      });
+    } on PlatformException catch (e) {
+      print(e);
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = 'Error - ${e.message}';
+      });
+      return;
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _authorized = authenticated ? 'Authorized' : 'Not Authorized';
+    });
+  }
+
+  void _cancelAuthentication() async {
+    await auth.stopAuthentication();
+    setState(() {
+      _isAuthenticating = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Biometric Auth'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Status : $_authorized\n'),
+              (_isAuthenticating)
+                  ? ElevatedButton(
+                      onPressed: _cancelAuthentication,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text('Canel Authentication'),
+                          Icon(Icons.cancel),
+                        ],
+                      ))
+                  : Column(
+                      children: [
+                        ElevatedButton(
+                            onPressed: _authenticate,
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text('Authenticate'),
+                                Icon(Icons.fingerprint),
+                              ],
+                            )),
+                      ],
+                    )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_biometric_auth/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,60 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Biometric Auth',
       theme: ThemeData(
      
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: home(title: 'Biometric Auth',),
     );
   }
 }
